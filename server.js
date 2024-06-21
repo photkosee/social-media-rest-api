4682ec327d2c352a2e1a233cfc0632a6c45a14d2
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-06-21 23:07:27 +1000

    fix: try new cors header to handle preflight requests issue

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3030;
-
-app.use(express.json());
-app.use(cors({
+const corsOptions = {
   origin: 'http://localhost:3000',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true
-}));
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
